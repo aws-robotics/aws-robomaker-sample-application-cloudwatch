341671f5eff7ff5cc0e47408fb668a74fd60bd70
Tim Robinson <tim@amazon.com>
2019-09-06 19:32:31 +0000

    Fix version, typos, package requirements
    - Major version bump to 2.0
    - Fix small typos in python scripts
    - Fix package.xml requirements
    - Fix health_metrics_config to have a float instead of int

--- robot_ws/src/cloudwatch_robot/setup.py
+++ robot_ws/src/cloudwatch_robot/setup.py
@@ -4,7 +4,7 @@ package_name = 'cloudwatch_robot'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='2.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     data_files=[
