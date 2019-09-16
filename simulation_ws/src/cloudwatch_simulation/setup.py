c3d79d76985246052378ff8a4f20cc8273888f56
Anas Abou Allaban <allabana@amazon.com>
2019-09-16 14:45:27 -0700

    Make Ament find the package

--- simulation_ws/src/cloudwatch_simulation/setup.py
+++ simulation_ws/src/cloudwatch_simulation/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'cloudwatch_simulation'
@@ -6,6 +8,7 @@ setup(
     name=package_name,
     version='2.0.0',
     data_files=[
+<<<<<<< HEAD
         ('share/' + package_name + '/launch', ['launch/bookstore_turtlebot_navigation.launch.py']),
         ('share/' + package_name + '/launch', ['launch/bookstore_turtlebot3_navigation.launch.py']),
         ('share/' + package_name + '/launch', ['launch/bookstore.launch.py']),
@@ -16,6 +19,12 @@ setup(
         ('share/' + package_name + '/worlds', ['worlds/empty.world']),
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+=======
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'param'), glob('param/*.yaml')),
+>>>>>>> Make Ament find the package
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -35,4 +44,3 @@ setup(
     ),
     license='Apache License, Version 2.0',
 )
-
