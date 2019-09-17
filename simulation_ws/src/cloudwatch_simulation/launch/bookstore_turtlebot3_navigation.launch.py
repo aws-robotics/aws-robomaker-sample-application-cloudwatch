f13416bf99949a8a7a90a94aab02eae74b431544
Tim Robinson <tim@amazon.com>
2019-09-17 14:37:25 -0700

    Move map to cloudwatch_simulation, add waffle to params
    - Move the map file from bookstore world to cloudwatch_simulation
    because the path was > 128 characters.
    - Create waffle and waffle_pi param files so they can be used for
    navigation.
    - Update map param in launch file to take the new map file correctly.

--- simulation_ws/src/cloudwatch_simulation/launch/bookstore_turtlebot3_navigation.launch.py
+++ simulation_ws/src/cloudwatch_simulation/launch/bookstore_turtlebot3_navigation.launch.py
@@ -74,7 +74,7 @@ def generate_launch_description():
                     'cloudwatch_simulation'), 'launch', 'turtlebot3_nav2.launch.py')
             ),
             launch_arguments={
-                'map_file': os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'maps', f"turtlebot3_{os.environ.get('TURTLEBOT3_MODEL', 'waffle_pi')}", 'map.yaml'),
+                'map': os.path.join(get_package_share_directory('cloudwatch_simulation'), 'maps', 'map.yaml'),
                 'initial_pose_x': launch.substitutions.LaunchConfiguration('x_pos'),
                 'initial_pose_y': launch.substitutions.LaunchConfiguration('y_pos'),
                 'initial_pose_a': launch.substitutions.LaunchConfiguration('yaw'),
--- simulation_ws/src/cloudwatch_simulation/setup.py
+++ simulation_ws/src/cloudwatch_simulation/setup.py
@@ -9,23 +9,11 @@ setup(
     version='2.0.0',
     packages=[package_name],
     data_files=[
-<<<<<<< HEAD
-        ('share/' + package_name + '/launch', ['launch/bookstore_turtlebot_navigation.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/bookstore_turtlebot3_navigation.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/bookstore.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/empty_world.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/turtlebot3_navigation.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/view_empty_world.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/turtlebot3_nav2.launch.py']),
-        ('share/' + package_name + '/worlds', ['worlds/empty.world']),
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-=======
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'param'), glob('param/*.yaml')),
->>>>>>> Make Ament find the package
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
