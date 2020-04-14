fcebb73b05d2653f6038d54da5997090f9e2f70b
Miaofei Mei <miaofei@amazon.com>
2020-04-14 12:07:22 -0700

    fix building and bundling of ros1 robomaker sample app (#2)
    * trigger pull request CI
    
    * add bundle step to CI
    
    * add bundle step to index.js
    
    * hack the generated index.js to try to make it work
    
    * install missing colcon bundle dependencies
    
    * add build and bundle for simulation_ws
    
    * test on only kinetic for now
    
    * attempt to fix colcon bundling failure
    
    * attempt to fix apt-key command issue
    
    * attempt to fix colcon bundling failure using different key
    
    * install and use curl
    
    * stop using packages-up-to qualifier
    
    * attempt to fix issue in dependency
    
    * pass more arguments in colcon build/bundle
    
    * fix simulation_ws build issue
    
    * remove failing "upload coverage" action

--- .github/actions/aws-robomaker-sample-application-ci/dist/index.js
+++ .github/actions/aws-robomaker-sample-application-ci/dist/index.js
@@ -960,11 +960,9 @@ const path = __importStar(__webpack_require__(622));
 const core = __importStar(__webpack_require__(470));
 const exec = __importStar(__webpack_require__(986));
 const fs = __webpack_require__(747);
-const COVERAGE_FOLDER_NAME = "coverage";
 const ROS_ENV_VARIABLES = {};
 const ROS_DISTRO = core.getInput('ros-distro', { required: true });
-// Optional parameter; Additional packages from a (optional) .rosinstall file will be appended.
-let PACKAGES_TO_SKIP_TESTS = core.getInput('packages-to-skip-tests');
+let PACKAGES = "none";
 function loadROSEnvVariables() {
     return __awaiter(this, void 0, void 0, function* () {
         const options = {
@@ -1008,34 +1006,29 @@ function getExecOptions(listenerBuffers) {
 // If .rosinstall exists, run 'rosws update' and return a list of names of the packages that were added.
 function fetchRosinstallDependencies() {
     return __awaiter(this, void 0, void 0, function* () {
-        let colconListBefore = { stdout: '', stderr: '' };
         let colconListAfter = { stdout: '', stderr: '' };
-        let packagesAddedViaRosws = [];
+        let packages = [];
         // Download dependencies not in apt if .rosinstall exists
         try {
             if (fs.existsSync(path.join(core.getInput('workspace-dir'), '.rosinstall'))) {
-                // Detect which packages were actually added by rosws, so we can skip testing/linting for them.
-                yield exec.exec("colcon", ["list", "--names-only"], getExecOptions(colconListBefore));
-                const packagesBefore = colconListBefore.stdout.split("\n");
                 yield exec.exec("rosws", ["update"], getExecOptions());
                 yield exec.exec("colcon", ["list", "--names-only"], getExecOptions(colconListAfter));
                 const packagesAfter = colconListAfter.stdout.split("\n");
                 packagesAfter.forEach(packageName => {
-                    if (!packagesBefore.includes(packageName)) {
-                        packagesAddedViaRosws.push(packageName.trim());
-                    }
+                    packages.push(packageName.trim());
                 });
             }
         }
         catch (err) {
             console.error(err);
         }
-        return Promise.resolve(packagesAddedViaRosws);
+        return Promise.resolve(packages);
     });
 }
 function setup() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            yield exec.exec("apt-key", ["adv", "--fetch-keys", "http://packages.osrfoundation.org/gazebo.key"]);
             const aptPackages = [
                 "cmake",
                 "lcov",
@@ -1043,26 +1036,22 @@ function setup() {
                 "python-pip",
                 "python-rosinstall",
                 "python3-colcon-common-extensions",
-                "python3-pip"
-            ];
-            const python2Packages = [
-                "coverage"
+                "python3-pip",
+                "python3-apt"
             ];
             const python3Packages = [
-                "setuptools"
+                "setuptools",
+                "colcon-bundle",
+                "colcon-ros-bundle"
             ];
             yield exec.exec("sudo", ["apt-get", "update"]);
             yield exec.exec("sudo", ["apt-get", "install", "-y"].concat(aptPackages));
-            yield exec.exec("sudo", ["pip", "install", "-U"].concat(python2Packages));
             yield exec.exec("sudo", ["pip3", "install", "-U"].concat(python3Packages));
             yield exec.exec("rosdep", ["update"]);
             yield loadROSEnvVariables();
             // Update PACKAGES_TO_SKIP_TESTS with the new packages added by 'rosws update'.
-            let packagesToSkipTests = yield fetchRosinstallDependencies();
-            if (PACKAGES_TO_SKIP_TESTS.length) {
-                packagesToSkipTests = packagesToSkipTests.concat(PACKAGES_TO_SKIP_TESTS.split(" "));
-            }
-            PACKAGES_TO_SKIP_TESTS = packagesToSkipTests.join(" ");
+            let packages = yield fetchRosinstallDependencies();
+            PACKAGES = packages.join(" ");
         }
         catch (error) {
             core.setFailed(error.message);
@@ -1073,108 +1062,18 @@ function build() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield exec.exec("rosdep", ["install", "--from-paths", ".", "--ignore-src", "-r", "-y", "--rosdistro", ROS_DISTRO], getExecOptions());
-            console.log(`Build step | packages-to-skip-tests: ${PACKAGES_TO_SKIP_TESTS}`);
-            let colconUpToCmakeArgs = [];
-            if (PACKAGES_TO_SKIP_TESTS.length) {
-                colconUpToCmakeArgs = ["--packages-up-to",].concat(PACKAGES_TO_SKIP_TESTS.split(" "));
-            }
-            yield exec.exec("colcon", ["build"].concat(colconUpToCmakeArgs), getExecOptions());
-            let colconCmakeArgs = [];
-            if (core.getInput('coverage')) {
-                if (PACKAGES_TO_SKIP_TESTS.length) {
-                    colconCmakeArgs = [
-                        "--packages-skip",
-                    ].concat(PACKAGES_TO_SKIP_TESTS.split(" "));
-                }
-                colconCmakeArgs = colconCmakeArgs.concat([
-                    "--cmake-args",
-                    "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
-                    "-DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage'",
-                    "-DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'"
-                ]);
-                yield exec.exec("colcon", ["build"].concat(colconCmakeArgs), getExecOptions());
-            }
+            console.log(`Build step | packages: ${PACKAGES}`);
+            yield exec.exec("colcon", ["build", "--build-base", "build", "--install-base", "install"], getExecOptions());
         }
         catch (error) {
             core.setFailed(error.message);
         }
     });
 }
-function test() {
+function bundle() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            if (!core.getInput('test')) {
-                console.log("Skipping testing as test flag is false");
-                return;
-            }
-            const workspaceDir = core.getInput('workspace-dir');
-            const packagesToTest = core.getInput('packages-to-test');
-            if (packagesToTest.length) {
-                const colconCmakeTestArgs = [
-                    "--packages-select",
-                ].concat(packagesToTest.split(" "), [
-                    "--cmake-target",
-                    "tests"
-                ]);
-                yield exec.exec("colcon", ["build"].concat(colconCmakeTestArgs), getExecOptions());
-            }
-            // Add the future install bin directory to PATH.
-            // This enables cmake find_package to find packages installed in the
-            // colcon install directory, even if local_setup.sh has not been sourced.
-            //
-            // From the find_package doc:
-            // https://cmake.org/cmake/help/latest/command/find_package.html
-            //   5. Search the standard system environment variables.
-            //   Path entries ending in /bin or /sbin are automatically converted to
-            //   their parent directories:
-            //   PATH
-            core.addPath(path.join(workspaceDir, "install", "bin"));
-            let colconArgs = [];
-            if (PACKAGES_TO_SKIP_TESTS.length) {
-                colconArgs = [
-                    "--event-handlers", "console_direct+",
-                    "--packages-skip",
-                ].concat(PACKAGES_TO_SKIP_TESTS.split(" "));
-            }
-            yield exec.exec("colcon", ["test"].concat(colconArgs), getExecOptions());
-            yield exec.exec("colcon", ["test-result", "--all", "--verbose"], getExecOptions());
-        }
-        catch (error) {
-            core.setFailed(error.message);
-        }
-    });
-}
-function coverage() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            if (!core.getInput('coverage')) {
-                console.log("Skipping producing code coverage as coverage flag is false");
-                return;
-            }
-            const packageLanguage = core.getInput('language');
-            const packagesToTest = core.getInput('packages-to-test');
-            const workspaceDir = core.getInput('workspace-dir');
-            const execOptions = getExecOptions();
-            if (packageLanguage == "cpp") {
-                yield exec.exec("lcov", ["--capture", "--directory", ".", "--output-file", "coverage.info"], execOptions);
-                yield exec.exec("lcov", ["--remove", "coverage.info", '/usr/*', '/opt/ros/*', '*/test/*', '--output-file', 'coverage.info'], execOptions);
-                yield exec.exec("lcov", ["--list", "coverage.info"], execOptions);
-                yield exec.exec("mkdir", ["-p", COVERAGE_FOLDER_NAME]);
-                yield exec.exec("cp", ["coverage.info", COVERAGE_FOLDER_NAME], execOptions);
-            }
-            else if (packageLanguage == "python") {
-                const allPackages = packagesToTest.split(RegExp('\\s'));
-                yield Promise.all(allPackages.map((packageName) => __awaiter(this, void 0, void 0, function* () {
-                    const packageExecOptions = getExecOptions();
-                    const workingDir = path.join(workspaceDir, 'build', packageName);
-                    packageExecOptions.cwd = workingDir;
-                    const coverageFileName = `coverage-${packageName}.info`;
-                    yield exec.exec("coverage", ["xml"], packageExecOptions);
-                    yield exec.exec("mv", ["coverage.xml", path.join(workspaceDir, COVERAGE_FOLDER_NAME, coverageFileName)], packageExecOptions);
-                    return coverageFileName;
-                })));
-            }
-            // Create coverage artifact for exporting
+            yield exec.exec("colcon", ["bundle", "--build-base", "build", "--install-base", "install", "--bundle-base", "bundle"], getExecOptions());
         }
         catch (error) {
             core.setFailed(error.message);
@@ -1185,13 +1084,10 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         yield setup();
         yield build();
-        yield test();
-        yield coverage();
+        yield bundle();
     });
 }
 run();
-
-
 /***/ }),
 
 /***/ 986:
