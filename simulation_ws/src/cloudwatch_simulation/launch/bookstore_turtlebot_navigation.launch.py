f76ff8a86e604a217b6755603237d81b787f0a58
Anson Wong <wongtsan@amazon.com>
2020-09-03 12:20:02 -0700

    Move robot_state_publisher from sim_ws to robot_ws

--- simulation_ws/src/cloudwatch_simulation/launch/bookstore_turtlebot_navigation.launch.py
+++ simulation_ws/src/cloudwatch_simulation/launch/bookstore_turtlebot_navigation.launch.py
@@ -8,9 +8,6 @@ from ament_index_python.packages import get_package_share_directory
 TURTLEBOT3_MODEL = os.environ.get('TURTLEBOT3_MODEL', 'burger')
 
 def generate_launch_description():
-    turtlebot_urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
-    turtlebot_urdf_file_path = os.path.join(get_package_share_directory('turtlebot3_description_reduced_mesh'), 'urdf', turtlebot_urdf_file_name)
-
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             name='x_pos',
@@ -52,18 +49,6 @@ def generate_launch_description():
                 'gui': launch.substitutions.LaunchConfiguration('gui'),
                 'gazebo_model_path': os.path.split(get_package_share_directory('turtlebot3_description_reduced_mesh'))[0],
             }.items()
-        ),
-        launch_ros.actions.Node(
-            package='robot_state_publisher',
-            node_executable='robot_state_publisher',
-            node_name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time')
-            }],
-            arguments=[
-                turtlebot_urdf_file_path
-            ]
         ),
         launch.actions.IncludeLaunchDescription(
             launch.launch_description_sources.PythonLaunchDescriptionSource(
