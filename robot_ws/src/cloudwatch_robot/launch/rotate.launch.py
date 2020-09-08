5cba32c2a93f7baf31450dd28c5310c67fe570f0
Anson Wong <ahtsans@gmail.com>
2020-09-08 13:45:37 -0700

    Move robot_state_publisher from sim_ws to robot_ws (#108)
    Moving ROS node robot_state_publisher from simulation workspace to robot workspace.

--- robot_ws/src/cloudwatch_robot/launch/rotate.launch.py
+++ robot_ws/src/cloudwatch_robot/launch/rotate.launch.py
@@ -4,8 +4,13 @@ import sys
 import launch
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
+ 
+TURTLEBOT3_MODEL = os.environ.get('TURTLEBOT3_MODEL', 'waffle_pi')
 
 def generate_launch_description():
+    turtlebot_urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
+    turtlebot_urdf_file_path = os.path.join(get_package_share_directory('turtlebot3_description_reduced_mesh'), 'urdf', turtlebot_urdf_file_name)
+
     launch_actions = [
         launch.actions.DeclareLaunchArgument(
             name='use_sim_time',
@@ -26,7 +31,18 @@ def generate_launch_description():
                     'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time')
                 }
             ]
-        )
+        ),
+        launch_ros.actions.Node(
+            package='robot_state_publisher',
+            node_executable='robot_state_publisher',
+            node_name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time')
+            }],
+            arguments=[
+                turtlebot_urdf_file_path
+            ])
     ]
     ld = launch.LaunchDescription(launch_actions)
     return ld
