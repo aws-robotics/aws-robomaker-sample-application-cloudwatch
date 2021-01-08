3c6eb48bf99fbbc1904c156a139124577b07e4a0
ojasjosh <ojasjosh@amazon.com>
2021-01-07 19:29:05 -0800

    Add linter scripts

---
+++ scripts/lint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ament_flake8 ../simulation_ws/src/aws_robomaker_simulation_common ../simulation_ws/src/cloudwatch_simulation ../robot_ws/src/cloudwatch_robot
+ament_pep257 ../simulation_ws/src/aws_robomaker_simulation_common ../simulation_ws/src/cloudwatch_simulation ../robot_ws/src/cloudwatch_robot
+ament_xmllint ../simulation_ws/src/aws_robomaker_simulation_common ../simulation_ws/src/cloudwatch_simulation ../robot_ws/src/cloudwatch_robot
