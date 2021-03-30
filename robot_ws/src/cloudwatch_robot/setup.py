4b5378aa69c33abed63958f087d259cf571896f9
ojasjosh <ojasjosh@amazon.com>
2021-03-30 14:39:29 -0700

    Adding logic to copy rviz from turtlebot3_navigation2

--- robot_ws/src/cloudwatch_robot/setup.py
+++ robot_ws/src/cloudwatch_robot/setup.py
@@ -1,12 +1,27 @@
 from setuptools import find_packages, setup
+from ament_index_python.packages import get_package_share_directory
+from setuptools.command.install import install
+from shutil import copyfile
+import os
 
 package_name = 'cloudwatch_robot'
+
+class CopyRvizModel(install):
+    def run(self):
+        src = get_package_share_directory('turtlebot3_navigation2')+'/rviz/tb3_navigation2.rviz'
+        dest_dir = '../../install/cloudwatch_robot/rviz'
+        os.mkdir(dest_dir)
+        copyfile(src, dest_dir+'/turtlebot3_navigation.rviz')
+        install.run(self)
 
 setup(
     name=package_name,
     version='2.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    cmdclass={
+        'install': CopyRvizModel,
+    },
     data_files=[
         ('share/' + package_name + '/launch',
          ['launch/await_commands.launch.py']),
@@ -28,9 +43,7 @@ setup(
          ['config/health_metrics_config.yaml']),
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/rviz',
-         ['rviz/turtlebot3_navigation.rviz']),
+        ('share/' + package_name, ['package.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
