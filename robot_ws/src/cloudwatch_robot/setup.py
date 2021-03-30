21dc04cc6368d0cb5c302b0d496adb18c2a2fb8f
ojasjosh <ojasjosh@amazon.com>
2021-03-30 10:38:19 -0700

    Adding rviz copy into robot_ws

--- robot_ws/src/cloudwatch_robot/setup.py
+++ robot_ws/src/cloudwatch_robot/setup.py
@@ -29,6 +29,8 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/rviz',
+         ['rviz/turtlebot3_navigation.rviz']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
