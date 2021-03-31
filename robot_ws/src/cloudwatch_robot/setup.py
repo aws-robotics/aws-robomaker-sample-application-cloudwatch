ada24eb721977318b387d41d8e4a9596ed799c72
Ojas Joshi <36244516+ojasjoshi@users.noreply.github.com>
2021-03-30 17:53:51 -0700

    add rviz model -- ros2 (#166)
    * Adding rviz model copy into robot_ws
    
    Co-authored-by: ojasjosh <ojasjosh@amazon.com>
    Co-authored-by: root <root@ue3d2e52451b858.ant.amazon.com>

--- robot_ws/src/cloudwatch_robot/setup.py
+++ robot_ws/src/cloudwatch_robot/setup.py
@@ -1,12 +1,34 @@
+import os
+from shutil import copyfile
+
+from ament_index_python.packages import get_package_share_directory
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+
 
 package_name = 'cloudwatch_robot'
+
+
+class CopyRvizModelToPackageDir(install):
+
+    def run(self):
+        rviz_file = get_package_share_directory('turtlebot3_navigation2') \
+            + '/rviz/tb3_navigation2.rviz'
+        dest_dir = 'rviz'  # this is relative to cloudwatch_robot package directory
+        if not os.path.isdir(dest_dir):
+            os.mkdir(dest_dir)
+        copyfile(rviz_file, dest_dir+'/turtlebot3_navigation.rviz')
+        install.run(self)
+
 
 setup(
     name=package_name,
     version='2.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    cmdclass={
+        'install': CopyRvizModelToPackageDir,
+    },
     data_files=[
         ('share/' + package_name + '/launch',
          ['launch/await_commands.launch.py']),
@@ -29,6 +51,7 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('rviz/', ['rviz/turtlebot3_navigation.rviz'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
